a718d785215e37b5825f23bbe971e600dc33539a
观烬 <2321833732@qq.com>
2026-08-09 23:43:47 +0800

    Add scripts/gh-login.ps1

---
+++ scripts/gh-login.ps1
@@ -0,0 +1,10 @@
+﻿$env:Path = "$env:LOCALAPPDATA\Programs\GitHubCLI\bin;$env:Path"
+Write-Host ""
+Write-Host "=== Embers iOS - GitHub Device Login ===" -ForegroundColor Yellow
+Write-Host "1. Copy the one-time code shown below"
+Write-Host "2. Press Enter to open browser: github.com/login/device"
+Write-Host "3. Sign in, enter code, click Authorize"
+Write-Host ""
+gh auth login --hostname github.com --git-protocol https --web
+Write-Host ""
+Write-Host "Login OK - you can close this window" -ForegroundColor Green
